18d7ebb89c2450602baab74fed7ac35d107d2a8a
Lautaro Carro <lautaroecarro@gmail.com>
2025-01-13 18:07:15 -0300

    Create init-postgis.sh

---
+++ scripts/init-postgis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "Initializing PostGIS extension..."
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS postgis;
+EOSQL
